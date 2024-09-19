2b824a25acd36d0ff2861637997ebd07b94bceaa
Zendiro <danilomenezes2009@hotmail.com>
2024-09-19 09:27:40 -0300

    commit

--- Ex031.py
+++ Ex031.py
@@ -1,7 +1,7 @@
-dist = int(input('Qual é a distância da viagem em KM?: '))
+dist = float(input('Qual é a distância da viagem em KM?: '))
 if dist <= 200:
     calckm1 = dist * 0.50
-    print('Você irá pagar R${}'.format(calckm1))
+    print('Você irá pagar R${:.2f}'.format(calckm1))
 else:
     calckm2 = dist * 0.45
-    print('Você irá pagar R${}'.format(calckm2))
+    print('Você irá pagar R${:.2f}'.format(calckm2))
--- Ex032.py
+++ Ex032.py
@@ -1,5 +1,10 @@
+from datetime import date
+
 ano = int(input('Digite o ano: '))
-if (ano%4==0 and ano%100!=0) or (ano%400==0):
+if ano == 0:
+    ano = date.today().year
+    print('O ano atual é: {}'.format(ano))
+if (ano % 4 == 0 and ano % 100!= 0) or (ano % 400== 0):
     print('Bissexto')
 else:
     print('Não é bissexto')
