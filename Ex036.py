7b1468818c5b80e0900078e5df19fbfc645142d3
Zendiro <danilomenezes2009@hotmail.com>
2024-09-20 14:37:58 -0300

    commit

---
+++ Ex036.py
@@ -0,0 +1,16 @@
+emp = float(input('Qual é o valor da casa? '))
+sal = float(input('Quanto é o seu salário? '))
+anos = int(input('Em quanto anos você irá pagar? '))
+pres = emp / (anos * 12)
+min = sal * 30 / 100
+if pres >= (sal - (sal * 30 / 100)):
+    print('Não foi dessa vez! A parcela ultrapassa 30% do seu salário, a parcela que vc iria pagar é de R${:.2f} por mês'.format(pres))
+else:
+    print('Parabéns! você pode fazer o financiamento, a parcela que vc irá pagar é de R${:.2f} por mês'.format(pres))
+
+
+
+
+
+    
+
