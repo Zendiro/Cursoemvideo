fafc26d65c635b82924a9acf2ffbe112e74daa6d
Zendiro <danilomenezes2009@hotmail.com>
2024-09-27 09:29:12 -0300

    commit

--- Aula014.py
+++ Aula014.py
@@ -12,6 +12,12 @@ while c < 10:
 print('Fim')'''
 
 n = 1
+par = impar = 0
 while n != 0:
     n = int(input('Digite um valor: '))
-print('Fim')
+    if n != 0:
+        if n % 2 == 0:
+            par += 1
+        else: 
+            impar += 1
+print(f'Você digitou {par} números pares e {impar} números ímpares')
--- test.py
+++ test.py
@@ -1,18 +1,19 @@
 from random import choice
 
-
+y = 's'
 escolha = str((input('Escolha entre pedra, papel e tesoura: '))).lower()
-opcaopc = ['pedra', 'papel', 'tesoura']
-escolhapc = choice(opcaopc)
-if escolha == escolhapc:
-    print ('Vocês empataram!')
-elif escolha == 'pedra' and escolhapc == 'papel':
-    print('Você perdeu! Você escolheu {} e o computador escolheu {}!'.format(escolha, escolhapc))
-elif escolha == 'tesoura' and escolhapc == 'pedra':
-    print('Você perdeu! Você escolheu {} e o computador escolheu {}!'.format(escolha, escolhapc))
-elif escolha == 'papel' and escolhapc == 'tesoura':
-    print('Você perdeu! Você escolheu {} e o computador escolheu {}!'.format(escolha, escolhapc))
-else:
-    print('Parabéns você ganhou! Você escolheu {} e o computador escolheu {}!'.format(escolha, escolhapc))
-while escolha == '':
-    
+while escolha == '' and y != 'n':
+    opcaopc = ['pedra', 'papel', 'tesoura']
+    escolhapc = choice(opcaopc) 
+    if escolha == escolhapc:
+        print('Vocês empataram!')
+    elif escolha == 'pedra' and escolhapc == 'papel':
+        print('Você perdeu! Você escolheu {} e o computador escolheu {}!'.format(escolha, escolhapc))
+    elif escolha == 'tesoura' and escolhapc == 'pedra':
+        print('Você perdeu! Você escolheu {} e o computador escolheu {}!'.format(escolha, escolhapc))
+    elif escolha == 'papel' and escolhapc == 'tesoura':
+        print('Você perdeu! Você escolheu {} e o computador escolheu {}!'.format(escolha, escolhapc))
+    else:
+        print('Parabéns você ganhou! Você escolheu {} e o computador escolheu {}!'.format(escolha, escolhapc))
+    y = str(input('Escolha inválida!\n Deseja jogar novamente?: ')).lower()
+print('Tudo bem! Estarei aqui a sua disposição ^^ ')
